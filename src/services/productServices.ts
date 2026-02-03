f0168cac42d39157141fc0f07635f1ed8b0d4234
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-04 01:28:12 +0530

    Add handle edge case while adding same exiting product then shows message instead of Adding

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -2,6 +2,10 @@ import { ProdcutType } from "../types/productType";
 import { Product } from "../models/product";
 
 export const createProductSerives = async (newProduct: ProdcutType) => {
+    const findProduct=await Product.findOne({Name:newProduct.Name})
+    if(findProduct){
+        return `This product already exists`
+    }
     const product = await new Product(newProduct);
     return await product.save()
 }
