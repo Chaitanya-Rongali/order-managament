31845d3ecd0c14f7ddf090ab45efb575f00ce8b8
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-04 01:09:58 +0530

    Add getProducts services functionality

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,8 +1,11 @@
 import { ProdcutType } from "../types/productType";
 import { Product } from "../models/product";
 
-export const createProductSerives=async (newProduct:ProdcutType)=>{
-    const product= await new Product(newProduct);
+export const createProductSerives = async (newProduct: ProdcutType) => {
+    const product = await new Product(newProduct);
     return await product.save()
-
+}
+export const getProductServices = async () => {
+    const products = await Product.find();
+    return products;
 }
