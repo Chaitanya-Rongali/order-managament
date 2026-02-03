27bd53b2891e437c7c07afc928f2756a129a8eb0
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-03 23:24:44 +0530

    Add database connection

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+const app = express();
+const port = process.env.PORT
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URL as string)
+.then((result) => {
+    console.log('connected to Mongodb');
+}).catch((err) => {
+    console.error(err);
+});
+app.listen(port, () => {
+    console.log(`app listening on ${port} `)
+})
+export default app
