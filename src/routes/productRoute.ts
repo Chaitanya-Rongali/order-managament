0077a0d5c12438f49366fabf1d54f8ec962de590
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-05 02:25:11 +0530

    Add route for get top selling orders

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import { createProductController, getProductController } from "../controllers/productController";
-import { getActiveOrderController, cancelOrderController, createOrderController, fetchTotalRevenue } from "../controllers/orderController";
+import { getActiveOrderController, cancelOrderController, createOrderController, fetchTotalRevenue, fetchTopSellingProducts } from "../controllers/orderController";
 export const router = express.Router()
 router.post('/addProduct', createProductController)
 router.get('/getProducts', getProductController)
 router.post('/addOrder', createOrderController)
 router.put('/cancelOrder/:id', cancelOrderController)
-router.get('/getActiveOrders',getActiveOrderController)
-router.get('/totalRevenue',fetchTotalRevenue)
+router.get('/getActiveOrders', getActiveOrderController)
+router.get('/totalRevenue', fetchTotalRevenue)
+router.get('/topSellingOrders', fetchTopSellingProducts)
