0eb4e4161568eb767ecdc8653bac057939857530
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-04 12:11:30 +0530

    Add add order route

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,5 +1,7 @@
 import express from "express"
 import { createProductController, getProductController } from "../controllers/productController";
+import { createOrderController } from "../controllers/orderController";
 export const router = express.Router()
 router.post('/addProduct',createProductController)
 router.get('/getProducts',getProductController)
+router.post('/addOrder',createOrderController)
