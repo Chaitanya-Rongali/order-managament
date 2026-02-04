3ec2df2b133a458f8b3acab5da117fc47438c8b6
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-04 15:41:18 +0530

    Add cancel order functionality  route

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,7 +1,8 @@
 import express from "express"
 import { createProductController, getProductController } from "../controllers/productController";
-import { createOrderController } from "../controllers/orderController";
+import { cancelOrderController, createOrderController } from "../controllers/orderController";
 export const router = express.Router()
-router.post('/addProduct',createProductController)
-router.get('/getProducts',getProductController)
-router.post('/addOrder',createOrderController)
+router.post('/addProduct', createProductController)
+router.get('/getProducts', getProductController)
+router.post('/addOrder', createOrderController)
+router.put('/cancelOrder/:id', cancelOrderController)
