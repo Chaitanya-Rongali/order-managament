689135ffa6e3466213f070c477c7b540cfab0ccf
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-05 01:58:37 +0530

    Add route for get total revenue

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import { createProductController, getProductController } from "../controllers/productController";
-import { getActiveOrderController, cancelOrderController, createOrderController } from "../controllers/orderController";
+import { getActiveOrderController, cancelOrderController, createOrderController, fetchTotalRevenue } from "../controllers/orderController";
 export const router = express.Router()
 router.post('/addProduct', createProductController)
 router.get('/getProducts', getProductController)
 router.post('/addOrder', createOrderController)
 router.put('/cancelOrder/:id', cancelOrderController)
 router.get('/getActiveOrders',getActiveOrderController)
+router.get('/totalRevenue',fetchTotalRevenue)
