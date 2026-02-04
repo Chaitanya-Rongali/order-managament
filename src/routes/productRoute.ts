3e06ef2d482c34a63b5f80a42bb88138400a705e
chaitanya <chaitanya.rongali@everest.engineering>
2026-02-04 16:18:40 +0530

    Add route for get active orders

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,8 +1,9 @@
 import express from "express"
 import { createProductController, getProductController } from "../controllers/productController";
-import { cancelOrderController, createOrderController } from "../controllers/orderController";
+import { getActiveOrderController, cancelOrderController, createOrderController } from "../controllers/orderController";
 export const router = express.Router()
 router.post('/addProduct', createProductController)
 router.get('/getProducts', getProductController)
 router.post('/addOrder', createOrderController)
 router.put('/cancelOrder/:id', cancelOrderController)
+router.get('/getActiveOrders',getActiveOrderController)
